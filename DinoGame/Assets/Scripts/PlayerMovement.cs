f2033eac7b681d8fa009043baefc15029f393b89
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-03-20 19:44:08 -0400

    Added Stuff

---
+++ DinoGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float moveBy = x * moveSpeed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+}
