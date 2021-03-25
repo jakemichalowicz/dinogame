9e08ded0ed889cc5e8836d734b8213b88a9ff444
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-03-24 20:53:19 -0400

    Movement Improvements

--- DinoGame/Assets/Scripts/PlayerMovement.cs
+++ DinoGame/Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [SerializeField] private LayerMask platformLayerMask;
+
+    public float moveSpeed;
+    public float jumpSpeed;
 
     private Rigidbody2D rb;
+    private BoxCollider2D bc;
     private Vector2 movement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
-        Move();
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+
     }
 
-    void Move()
+    void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float moveBy = x * moveSpeed;
-        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+        //Player X Movement
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        float extraHeightTest = .05f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, extraHeightTest, platformLayerMask);
+
+        return raycastHit.collider != null;
     }
 }
