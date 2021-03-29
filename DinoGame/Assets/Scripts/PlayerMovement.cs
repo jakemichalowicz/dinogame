5d96073b8d0c04fd60745af98b0e1d88c4d7c61d
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-03-28 23:06:53 -0400

    movement changes

--- DinoGame/Assets/Scripts/PlayerMovement.cs
+++ DinoGame/Assets/Scripts/PlayerMovement.cs
@@ -1,59 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField] private LayerMask platformLayerMask;
+public class PlayerMovement : MonoBehaviour {
 
-    public float moveSpeed;
-    public float jumpSpeed;
+	public CharacterController2D controller;
 
-    private Rigidbody2D rb;
-    private BoxCollider2D bc;
-    private Vector2 movement;
+	public float runSpeed = 40f;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        bc = GetComponent<BoxCollider2D>();
-    }
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	
+	// Update is called once per frame
+	void Update () {
 
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-        {
-            rb.velocity = Vector2.up * jumpSpeed;
-        }
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-    }
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
 
-    void FixedUpdate()
-    {
-        //Player X Movement
-        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
-        if(Input.GetKey(KeyCode.A))
-        {
-            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
-        }
-        else
-        {
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-            }
-            else
-            {
-                rb.velocity = new Vector2(0, rb.velocity.y);
-            }
-        }
-    }
+	}
 
-    private bool IsGrounded()
-    {
-        float extraHeightTest = .05f;
-        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, extraHeightTest, platformLayerMask);
-
-        return raycastHit.collider != null;
-    }
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
