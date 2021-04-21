72c63446d69f7e79abf6a09153d2bb35f22a245c
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-04-21 18:40:24 -0400

    Major Controller Overhaul

--- DinoGame/Assets/Scripts/CameraFollow.cs
+++ DinoGame/Assets/Scripts/CameraFollow.cs
@@ -4,11 +4,53 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
+	public float FollowSpeed = 2f;
+	public Transform Target;
 
-    void Update()
-    {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
-    }
+	// Transform of the camera to shake. Grabs the gameObject's transform
+	// if null.
+	private Transform camTransform;
+
+	// How long the object should shake for.
+	public float shakeDuration = 0f;
+
+	// Amplitude of the shake. A larger value shakes the camera harder.
+	public float shakeAmount = 0.1f;
+	public float decreaseFactor = 1.0f;
+
+	Vector3 originalPos;
+
+	void Awake()
+	{
+		Cursor.visible = false;
+		if (camTransform == null)
+		{
+			camTransform = GetComponent(typeof(Transform)) as Transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		originalPos = camTransform.localPosition;
+	}
+
+	private void Update()
+	{
+		Vector3 newPosition = Target.position;
+		newPosition.z = -10;
+		transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+
+		if (shakeDuration > 0)
+		{
+			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+			shakeDuration -= Time.deltaTime * decreaseFactor;
+		}
+	}
+
+	public void ShakeCamera()
+	{
+		originalPos = camTransform.localPosition;
+		shakeDuration = 0.2f;
+	}
 }
