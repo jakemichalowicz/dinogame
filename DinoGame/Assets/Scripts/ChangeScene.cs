26866bb4439d18ec3691645a18231f6ea649e51f
Jake Michalowicz <jjm590@scarletmail.rutgers.edu>
2021-04-30 23:53:07 -0400

    Animation and Level Work

---
+++ DinoGame/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public Collider2D playerCollider;
+    public int linkedSceneIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision == playerCollider)
+        {
+            SceneManager.LoadScene(linkedSceneIndex);
+        }
+    }
+}
